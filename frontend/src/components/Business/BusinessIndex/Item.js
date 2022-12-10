e7e98e2b3cc2f50783563cb04b7dcc6892ec227d
Qiao Yang Han <qyhwork@gmail.com>
2022-12-10 02:40:41 -0500

    Refined on business index page again

--- frontend/src/components/Business/BusinessIndex/Item.js
+++ frontend/src/components/Business/BusinessIndex/Item.js
@@ -2,6 +2,10 @@ import "./Item.css";
 import { EST_OFFSET, toLocalTime, isOpen } from "../../../store/time.js";
 
 const Item = ({ idx, business }) => {
+    const categorySpans = business.category.split(",").map(c => (
+        <span className="category">{c.trim()}</span>
+    ));
+
     return (
         <div className="business-index-item">
             <div className="business-index-item-picture">
@@ -11,19 +15,20 @@ const Item = ({ idx, business }) => {
                 <h1>{`${idx + 1}. ${business.name}`}</h1>
             </div>
             <div>
-                <span>{business.category}</span>
-                <span>{business.price}</span>
-                <span className="icon-circle-container"><i className="fas fa-circle"></i></span>
+                {/* <span>{business.category}</span> */}
+                {categorySpans}
+                <span style={{ color: "#727272" }}>{business.price}</span>
+                <span className="icon-circle-container" style={{ color: "#727272" }}><i className="fas fa-circle"></i></span>
             </div>
             {isOpen(business) ? (
             <div>
                 <span className="business-open">Open</span>
-                <span>until {toLocalTime(business.close, EST_OFFSET)}</span>
+                <span style={{fontWeight: "300"}}>until {toLocalTime(business.close, EST_OFFSET)}</span>
             </div>
             ) : (
             <div>
                 <span className="business-closed">Closed</span>
-                <span>until {toLocalTime(business.open, EST_OFFSET)}</span>
+                <span style={{ fontWeight: "300" }}>until {toLocalTime(business.open, EST_OFFSET)}</span>
             </div>
             )}
         </div>
