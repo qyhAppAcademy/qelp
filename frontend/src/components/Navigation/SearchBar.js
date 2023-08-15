1e368c2ede7a18de5f8d8830fad8064b993f7204
Qiao Yang Han <qyhwork@gmail.com>
2023-08-14 22:13:47 -0400

    Working on button animation

--- frontend/src/components/Navigation/SearchBar.js
+++ frontend/src/components/Navigation/SearchBar.js
@@ -15,10 +15,14 @@ const SearchBar = ({ setQuery, setAddressQuery }) => {
         const circle = document.createElement("span");
         const diameter = Math.max(button.clientWidth, button.clientHeight);
         const radius = diameter / 2;
-
+        
         circle.style.width = circle.style.height = `${diameter}px`;
-        circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-        circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+        circle.style.top = `${event.clientY - button.offsetTop - 30 - radius}px`;
+        circle.style.left = `${event.clientX - button.offsetLeft - 180 - radius}px`;
+        // circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+        // circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+        console.log(circle.style.top);
+        console.log(circle.style.left);
         circle.classList.add("ripple");
         
         const ripple = button.getElementsByClassName("ripple")[0];
