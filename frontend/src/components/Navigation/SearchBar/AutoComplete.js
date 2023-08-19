0a0c6ab323bb65625e373e7dad41d1b130d31c0e
Qiao Yang Han <qyhwork@gmail.com>
2023-08-19 06:24:33 -0400

    Working on Google Place Autocomplete API - Having some success on location strictions

--- frontend/src/components/Navigation/SearchBar/AutoComplete.js
+++ frontend/src/components/Navigation/SearchBar/AutoComplete.js
@@ -8,15 +8,22 @@ const GOOGLE_API_SCRIPTS = {
 
 const Autocomplete = () => {
     useExternalScripts(GOOGLE_API_SCRIPTS);
-
     let autocomplete;
+    const center = { lat: 40.689362, lng: -73.986638 };
+    const defaultBounds = {
+        north: center.lat + 0.2,
+        south: center.lat - 0.2,
+        east: center.lng + 0.2,
+        west: center.lng - 0.2,
+    };
     const countryRestrict = { 
         country: "us" 
     };
     const options = {
-        types: ["(cities)"],
+        bounds: defaultBounds,
+        // types: ["(cities)"],
         componentRestrictions: countryRestrict,
-        fields: ["icon"],
+        fields: ["address_components", "geometry", "icon", "name"],
         strictBounds: true
     };
 
