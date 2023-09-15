c23ee0434daa405e90090567e6612f9d4e773c2d
Qiao Yang Han <qyhwork@gmail.com>
2023-09-14 22:56:33 -0400

    Refining Nav 4

--- frontend/src/components/Navigation/Session/User/Profile/index.js
+++ frontend/src/components/Navigation/Session/User/Profile/index.js
@@ -1,5 +1,30 @@
-<button onClick={openMenu} className='profile-icon'>
-    <span>
-        <i style={{ fontSize: "36px" }} className="fas fa-user-circle"></i>
-    </span>
-</button>
+import { useEffect } from "react";
+
+const Profile = ({ showMenu, setShowMenu }) => {
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+    };
+
+    const closeMenu = () => {
+        setShowMenu(false);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    return (
+        <button onClick={openMenu} className='profile-icon'>
+            <span>
+                <i style={{ fontSize: "36px" }} className="fas fa-user-circle"></i>
+            </span>
+        </button>
+    );
+}
+
+export default Profile;
--- frontend/src/components/Navigation/Session/User/index.js
+++ frontend/src/components/Navigation/Session/User/index.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
+import Profile from "./Profile";
 import Menu from "./Menu";
 import "./index.css";
 
 const User = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
         <div className='profile-button'>
-
+            <Profile showMenu={showMenu} setShowMenu={setShowMenu} />
             {showMenu && <Menu />}
         </div>
     );
