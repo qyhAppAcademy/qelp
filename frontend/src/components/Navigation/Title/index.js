02d63ea68e92a08ff37c7edb7e62d56d832922ff
Qiao Yang Han <qyhwork@gmail.com>
2023-09-17 02:43:36 -0400

    Working on GoogleMap

--- frontend/src/components/Navigation/Title/index.js
+++ frontend/src/components/Navigation/Title/index.js
@@ -1,12 +1,21 @@
 import "./index.css";
+import { useQueryContext } from "../../../context/Query";
 import { useHistory } from "react-router-dom";
 
 const Title = () => {
+    const { setKeywordQuery, setAddressQuery } = useQueryContext();
     const history = useHistory();
 
     return (
         <div className="title">
-            <button onClick={() => history.push("/")}>
+            <button onClick={() => {
+                setKeywordQuery("");
+                setAddressQuery({
+                    val: "",
+                    geo: null
+                });
+                history.push("/");
+            }}>
                 qelp<i className="fab fa-yelp"></i>
             </button>
         </div>
