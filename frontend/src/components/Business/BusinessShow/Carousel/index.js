50e7037b8f4561b08aa592d182076d4e3472bd0f
Qiao Yang Han <qyhwork@gmail.com>
2023-09-20 16:22:58 -0400

    Working on Carousel

--- frontend/src/components/Business/BusinessShow/Carousel/index.js
+++ frontend/src/components/Business/BusinessShow/Carousel/index.js
@@ -8,44 +8,58 @@ const Carousel = ({ business }) => {
     const rightArrowRef = useRef();
 
     const slidesRef = useRef();
+    const lefts = useRef(0);
 
-    const transition = (direction) => {
-        leftArrowRef.current.classList.remove("inactive");
-        rightArrowRef.current.classList.remove("inactive");
+    const loadLefts = () => {
+        lefts.current += 1;
+        if (lefts.current < business.photoUrls.length)
+            return;
 
-        const arrow = direction === "left" ?
-            leftArrowRef.current : rightArrowRef.current;
-
+        lefts.current = [0];
         const imgs = slidesRef.current.children;
-        const left = slidesRef.current.style.left.slice(0, -2);
-        const lefts = [0];
 
         let accumulator = 0;
         for (let i = 0; i <= imgs.length; i++) {
             if (window.innerWidth < accumulator)
-                lefts.unshift(window.innerWidth - accumulator);
+                lefts.current.unshift(window.innerWidth - accumulator);
             if (i < imgs.length)
                 accumulator += imgs[i].clientWidth;
         }
 
-        const newLeft = direction === "left" ?
-            lefts.find(ele => ele > left) :
-            [...lefts].reverse().find(ele => ele < left);
+        if (lefts.current.length > 1)
+            rightArrowRef.current.classList.remove("inactive");
+    };
 
-        if (newLeft !== undefined)
-            slidesRef.current.style.left = `${newLeft}px`;
-        if (!newLeft || newLeft === 0 || newLeft === lefts[0])
+    const transition = (direction) => {
+        const arrow = direction === "left" ?
+            leftArrowRef.current : rightArrowRef.current;
+
+        if (arrow.classList.contains("inactive")) return;
+
+        leftArrowRef.current.classList.remove("inactive");
+        rightArrowRef.current.classList.remove("inactive");
+
+        const left = slidesRef.current.style.left.slice(0, -2);
+
+        const newLeft = direction === "left" ?
+            lefts.current.find(ele => ele > left) :
+            [...lefts.current].reverse().find(ele => ele < left);
+
+        slidesRef.current.style.left = `${newLeft}px`;
+
+        if (newLeft === 0 || newLeft === lefts.current[0])
             arrow.classList.add("inactive");
 
         console.log("Screen Width: " + window.innerWidth);
         console.log("Slides Left: " + left);
         console.log("Slides New Left: " + newLeft);
-        console.log(lefts);
+        console.log(lefts.current);
     };
 
     const arrows = DIRECTIONS.map((DIRECTION, idx) => (
         <button
             ref={DIRECTION === "left" ? leftArrowRef : rightArrowRef}
+            className="inactive"
             onClick={() => transition(DIRECTION)}
             key={idx}
         >
@@ -54,17 +68,18 @@ const Carousel = ({ business }) => {
     ));
 
     const slides = business.photoUrls.map((photo, idx) => (
-        <img src={`${photo.url}`} alt={`slides-img-${idx}`} key={idx} />
+        <img
+            src={`${photo.url}`}
+            alt={`slides-img-${idx}`}
+            onLoad={loadLefts}
+            key={idx}
+        />
     ));
 
     return (
         <>
             <div className="arrows">{arrows}</div>
-            <div
-                ref={slidesRef}
-                className="slides"
-                style={{ left: 0 }}
-            >
+            <div ref={slidesRef} className="slides" style={{ left: 0 }}>
                 {slides}
             </div>
         </>
