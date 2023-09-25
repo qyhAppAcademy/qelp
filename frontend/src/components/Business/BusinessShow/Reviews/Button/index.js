e654841beb2bd4d9732376f8ee2904c443fffcea
Qiao Yang Han <qyhwork@gmail.com>
2023-09-25 07:23:27 -0400

    Working on CRUD Buttons in Reviews

--- frontend/src/components/Business/BusinessShow/Reviews/Button/index.js
+++ frontend/src/components/Business/BusinessShow/Reviews/Button/index.js
@@ -1,17 +1,17 @@
-import "./ReviewButtons.css";
+import "./index.css";
 
-const ReviewButtons = ({ hasReviewed, setShowReviewForm }) => {
+const Button = ({ reviewed, setShowReviewForm }) => {
 
     return (
         <div className="business-review-buttons">
             <button className="submit-review-button" onClick={(e) => setShowReviewForm(true)}>
                 <a href="#review-form-location">
                     <span style={{padding: "0 6px 0 0"}}><i className="fas fa-star"></i></span>
-                    <span>{hasReviewed ? "Edit your" : "Write a"} review</span>
+                    <span>{reviewed ? "Edit your" : "Write a"} review</span>
                 </a>
             </button>
         </div>
     );
 }
 
-export default ReviewButtons;
+export default Button;
--- frontend/src/components/Business/BusinessShow/Reviews/index.js
+++ frontend/src/components/Business/BusinessShow/Reviews/index.js
@@ -1,43 +1,43 @@
-import "./Reviews.css";
-
+import "./index.css";
 import { useSelector } from "react-redux";
 import { getCurrentUser } from "../../../../store/session";
 import { useState } from "react";
-
-import ReviewButtons from "./ReviewButtons";
-import ReviewForm from "../../../Review/Form";
 import Review from "../../../Review";
+
+import ReviewButtons from "./Button";
+import ReviewForm from "../../../Review/Form";
+import Button from "./Button";
 
 const Reviews = ({ business }) => {
     const user = useSelector(getCurrentUser);
 
     const reviews = Object.values(business.reviews).map((review, idx) => (
-        <div key={review.id} id={`review-${review.id}`}>
+        <div id={`review-${review.id}`} key={idx}>
             <div className="review-header">
                 <h1>{`${review.user.email.split("@")[0]}...`}</h1>
             </div>
-            <Review key={review.id} review={review} />
+            <Review review={review} />
         </div>
     ));
 
     const review = user ? Object.values(business.reviews)
         .find(review => review.user.id === user.id) : null;
 
-    const reviewed = review !== null;
-
     const [showReviewForm, setShowReviewForm] = useState(false);
 
     return (
         <div className="business-reviews-container">
-            {currentUser && (
-                <ReviewButtons hasReviewed={hasReviewed} setShowReviewForm={setShowReviewForm} />
-            )}
-            <h1>Reviews</h1>
+            {user &&
+                <Button
+                    reviewed={review !== null}
+                    setShowReviewForm={setShowReviewForm}
+                />
+            }
+            {/* <h1>Reviews</h1>
             {reviews}
-            {/* <BusinessReviews business={business} /> */}
-            {currentUser && (
+            {user && (
                 <ReviewForm businessId={business.id} review={review} showReviewForm={showReviewForm} setShowReviewForm={setShowReviewForm} />
-            )}
+            )} */}
         </div>
     );
 };
